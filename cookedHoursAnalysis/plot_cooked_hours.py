d9e446043720b18ccc22a8dc92967d5bb3cfc8e0
Daniel Fadeev <mdanielfadeev@gmail.com>
2025-02-01 21:05:41 +0100

    fix

--- cookedHoursAnalysis/plot_cooked_hours.py
+++ cookedHoursAnalysis/plot_cooked_hours.py
@@ -9,7 +9,7 @@ def load_and_prepare_data(filename='cookedHoursAnalysis/interval_pnls.json'):
         data = json.load(f)
     
     df = pd.DataFrame(data)
-    df['datetime'] = pd.to_datetime(df['startTimestamp'], unit='ms')
+    df['datetime'] = pd.to_datetime(df['startTimestamp'], unit='ms', origin='unix', tz='UTC+1')
     return df
 
 def plot_total_pnls(df, output_path='cookedHoursAnalysis/total_pnl_analysis.png'):
